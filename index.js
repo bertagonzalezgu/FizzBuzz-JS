0d135257a88b0b4428b0c00b0a56d93522c9a0f7
Berta González <berta.gonzalez99@gmail.com>
2026-06-09 10:45:22 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/* To Do:
+- Crear "form" html con input
+- Crear boton
+- Crear div con id para respuesta
+- Crear 1 arrays (historial de respuestas)
+- Crear variables (número ingresado en input, r1: Fizz, r2: Buzz, r3: r1 + r2, mensaje de error) para la respuesta
+- Validación de campo input (validar que sea número válido) con condicional if
+- Mandar mensaje de error en caso de no ser número válido
+- Crear lógica, con condicional if con el resultado correspondiente
+- Crear objeto con 2 values: número input + resultado correspondiente
+*/
+
