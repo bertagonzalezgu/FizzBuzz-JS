bbb9902c98b49953b0f6a22b0ae8884429fc4135
Berta González <berta.gonzalez99@gmail.com>
2026-06-10 15:02:26 +0200

    feat: Start file test

---
+++ tests/fizzbuzz.test.js
@@ -0,0 +1,12 @@
+import { describe, test, expect } from "vitest";
+
+import { showResult } from "../src/scripts/fizzbuzz-ui";
+
+describe('Validación de FizzBuzz', () => {
+
+  test('Given un número 9 / When el número es procesado / Then se muestra "Fizz"', () => {
+    
+
+    
+  });
+});
